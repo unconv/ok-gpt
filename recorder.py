d20c5134de89c548f0489470ab71c40206bddf3b
unconv <unconventionalcoding@gmail.com>
2023-11-20 06:13:55 +0200

    added ambient noise detection

--- recorder.py
+++ recorder.py
@@ -5,8 +5,13 @@ import wave
 import os
 
 whisper_model = whisper.load_model("base")
+ambient_detected = False
+speech_volume = 100
 
 def live_speech(wait_time=10):
+    global ambient_detected
+    global speech_volume
+
     FORMAT = pyaudio.paInt16
     CHANNELS = 1
     RATE = 44100
@@ -31,7 +36,20 @@ def live_speech(wait_time=10):
         data = stream.read(CHUNK)
         rms = audioop.rms(data, 2)
 
-        if rms > 300:
+        if not ambient_detected:
+            if frames_recorded < 40:
+                if frames_recorded == 1:
+                    print("Detecting ambient noise...")
+                if frames_recorded > 5:
+                    if speech_volume < rms:
+                        speech_volume = rms
+                continue
+            elif frames_recorded == 40:
+                print("Listening...")
+                speech_volume = speech_volume * 3
+                ambient_detected = True
+
+        if rms > speech_volume:
             recording = True
             frames_recorded = 0
         elif recording and frames_recorded > wait_time:
